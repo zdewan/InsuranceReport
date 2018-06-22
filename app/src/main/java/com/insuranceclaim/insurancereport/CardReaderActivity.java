c115341bd54c07c2b78e815791e14c1deecfa503
Kyrel Jerome <kyrel2007@gmail.com>
2018-06-21 21:34:44 -0400

    Added a CardReaderActivity, meant to be the interface where the user scrolls through all current cards and insurables

---
+++ app/src/main/java/com/insuranceclaim/insurancereport/CardReaderActivity.java
@@ -0,0 +1,13 @@
+package com.insuranceclaim.insurancereport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_reader);
+    }
+}
