f87c1b3824430ce236a2031fc40fa912f6d49de2
Kyrel Jerome <kyrel2007@gmail.com>
2018-06-22 18:37:35 -0400

    Fixed misplaced ">" in CardReaderActivity's gridlayout

---
+++ app/src/main/java/com/insuranceclaim/insurancereport/com/inusranceclaim/insurancereport/CardUtils/AutomobileInsurableCard.java
@@ -0,0 +1,9 @@
+package com.insuranceclaim.insurancereport.com.inusranceclaim.insurancereport.CardUtils;
+
+/**
+ * Created by kyrel_000 on 2018-06-21.
+ * Originally created in InsuranceReport.
+ */
+
+public class AutomobileInsurableCard extends InsurableCard{
+}
---
+++ app/src/main/java/com/insuranceclaim/insurancereport/com/inusranceclaim/insurancereport/CardUtils/InsurableCard.java
@@ -0,0 +1,12 @@
+package com.insuranceclaim.insurancereport.com.inusranceclaim.insurancereport.CardUtils;
+
+import android.support.v7.widget.CardView;
+
+/**
+ * Created by kyrel_000 on 2018-06-21.
+ * Originally created in InsuranceReport.
+ */
+
+public class InsurableCard extends CardView{
+
+}
